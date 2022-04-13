e045226acf66cebba78869f1dc8aa219eb6789a4
baidikaroy29 <89181284+baidikaroy29@users.noreply.github.com>
2022-04-13 19:46:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+
+	img = loadImage("mario05.png");
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+
+	createCanvas(650,400);
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent("game_console");
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+	console.log("Model Loaded !!!");
+}
+
+function draw() {
+	background = "#D3D3D3";
+
+	if (noseX < 300)
+	{
+		marioX = marioX - 1;
+	}
+
+	if (noseX > 300)
+	{
+		marioX = marioX + 1;
+	}	
+
+	if (noseY < 150)
+	{
+		marioY = marioY - 1;
+	}
+
+	image(img, marioX, marioY, 40, 70);
+	
+	
+	game();
+}
+
+function gotPoses(results)
+{
+	if (results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+
+
